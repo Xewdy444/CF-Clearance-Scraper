8ea4a35a5c4fa6140c875e0c68949a03535038c8
Xewdy <95155966+Xewdy444@users.noreply.github.com>
2022-04-17 19:11:13 -0500

    Upload files

---
+++ README.md
@@ -0,0 +1,44 @@
+# CF-Clearance-Scraper
+
+A simple program for fetching cf_clearance cookies from websites issuing cloudflare challanges to users. This program works on JavaScript and (non-interactive) managed challenges. It does not work on legacy CAPTCHA challenges, since human interraction would  be required.
+
+## Installation
+
+```
+pip3 install -r requirements.txt
+```
+
+#### Mac | Windows
+
+```
+npm install playwright
+```
+
+#### Linux
+
+```
+npm install playwright
+playwright install-deps
+```
+
+## Usage
+
+```
+usage: cf_cookie.py [-h] [-v] [-u URL] [-f FILE] [-t TIMEOUT] [-p PROXY]
+
+Fetches cf_clearance cookies from websites issuing cloudflare challanges to users
+
+options:
+  -h, --help            show this help message and exit
+  -v, --verbose         Enable verbose logging
+  -u URL, --url URL     URL to fetch cf_clearance cookie from
+  -f FILE, --file FILE  File to write the cf_clearance cookie value to
+  -t TIMEOUT, --timeout TIMEOUT
+                        Cookie fetch timeout (milliseconds)
+  -p PROXY, --proxy PROXY
+                        Proxy server to use for requests (Authentification not
+                        supported). Example: socks5://172.66.43.144
+```
+```
+Example: python3 cf_cookie.py -u https://nowsecure.nl -f cookies.txt -v
+```
---
+++ cf_cookie.py
@@ -0,0 +1,110 @@
+import asyncio
+import argparse
+import requests
+from playwright.async_api import async_playwright
+
+async def main():
+
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+
+    parser = argparse.ArgumentParser(description="Fetches cf_clearance cookies from websites issuing cloudflare challanges to users")
+    parser.add_argument("-v", "--verbose", help="Enable verbose logging", action="store_true")
+    parser.add_argument("-u", "--url", help="URL to fetch cf_clearance cookie from", type=str, default=None)
+    parser.add_argument("-f", "--file", help="File to write the cf_clearance cookie value to", type=str, default=None)
+    parser.add_argument("-t", "--timeout", help="Cookie fetch timeout (milliseconds)", type=int, default=5000)
+    parser.add_argument("-p", "--proxy", help="Proxy server to use for requests (Authentification not supported). Example: socks5://172.66.43.144", type=str, default=None)
+    args = parser.parse_args()
+
+    if args.url is None:
+        parser.print_help()
+        exit()
+
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-us",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": user_agent
+    }
+
+    try:
+        if args.proxy:
+            proxy = {
+                "http": args.proxy,
+                "https": args.proxy
+            }
+            init_request = requests.get(args.url, headers=headers, proxies=proxy)
+        else:
+            init_request = requests.get(args.url, headers=headers)
+    except Exception as e:
+        if args.verbose is True:
+            print(f"[!] {e}")
+        exit()
+
+    cf_challenge_html = ["<title>Please Wait... | Cloudflare</title>", "<title>Attention Required! | Cloudflare</title>", "Checking your browser before accessing"]
+
+    if any(x in init_request.text for x in cf_challenge_html):
+        if args.verbose is True:
+            print("[+] Cloudflare challenge detected. Attempting to fetch cf_clearance cookie...")
+    else:
+        if args.verbose is True:
+            print("[!] Cloudflare challenge not detected. Exiting...")
+        exit()
+
+    async with async_playwright() as p:
+        if args.verbose is True:
+            print("[+] Launching headless browser...")
+
+        if args.proxy:
+            browser = await p.webkit.launch(headless=True, proxy={"server": args.proxy})
+        else:
+            browser = await p.webkit.launch(headless=True)
+
+        try:
+            context = await browser.new_context(user_agent=user_agent)
+            page = await context.new_page()
+            await page.goto(args.url, wait_until="networkidle")
+            await page.wait_for_timeout(args.timeout)
+        except Exception as e:
+            if args.verbose is True:
+                print("[!] {}".format(str(e).split("\n")[0]))
+                print("[+] Closing headless browser...")
+            await browser.close()
+            exit()
+
+        cookies = await page.context.cookies()
+        cf_clearance_cookie = [cookie for cookie in cookies if cookie['name'] == 'cf_clearance']
+        
+        if len(cf_clearance_cookie) != 0:
+            if args.verbose is True:
+                print(f"[+] Cookie: cf_clearance={cf_clearance_cookie[0]['value']}")
+            elif args.verbose is False:
+                print(cf_clearance_cookie[0]['value'])
+
+            if args.file:
+                try:
+                    with open(args.file, "x") as file:
+                        if args.verbose is True:
+                            print(f"[+] Writing cf_clearance cookie value to {args.file}...")
+                        file.write(cf_clearance_cookie[0]['value'] + "\n")
+                except FileExistsError:
+                    with open(args.file, "a") as file:
+                        if args.verbose is True:
+                            print(f"[+] Writing cf_clearance cookie value to {args.file}...")
+                        file.write(cf_clearance_cookie[0]['value'] + "\n")
+                except Exception as e:
+                    print(f"[!] {e}")
+        else:
+            if args.verbose is True:
+                print("[!] Failed to retrieve cf_clearance cookie.")
+
+        if args.verbose is True:
+            print("[+] Closing headless browser...")
+        await browser.close()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        exit()
