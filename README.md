9969c5d449557bcf198abc41822c9dc5365b71f6
Xewdy <95155966+Xewdy444@users.noreply.github.com>
2022-08-18 14:59:28 -0500

    Changes
    - Switched to synchronous playwright
    - Added support for proxy authentication
    - Added interactive managed challenge bypass
    - Other small changes (Code cleanup)

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # CF-Clearance-Scraper
 
-A simple program for fetching cf_clearance cookies from websites issuing cloudflare challenges to users. This program works on JavaScript and (non-interactive) managed challenges. 
-
-#### Please Note
-This scraper cannot solve CAPTCHA challenges since human interaction would be required.
+A simple program for fetching cf_clearance cookies from websites issuing cloudflare challenges to users. This program works on JavaScript and managed challenges. 
 
 ## Clearance Cookie Usage
 In order to bypass cloudflare challenges with the cloudflare clearance cookies, you must make sure of two things:
 
-- The user agent used to fetch the clearance cookie must match the user agent being used within the requests that use the clearance cookie (You can edit the user agent used by the scraper by changing the ```user_agent``` variable on line 10)
+- The user agent used to fetch the clearance cookie must match the user agent being used within the requests that use the clearance cookie (You can edit the user agent used by the scraper by changing the ```USER_AGENT``` variable)
 - The IP address used to fetch the clearance cookie must match the IP address being used to make the requests that use the clearance cookie
 
 ```mermaid
@@ -23,7 +20,7 @@ flowchart
 ## Installation
 
     $ pip install -r requirements.txt
-    $ playwright install --with-deps webkit
+    $ playwright install --with-deps firefox
 
 
 ## Usage
@@ -36,12 +33,11 @@ options:
   -h, --help            show this help message and exit
   -v, --verbose         Enable verbose logging
   -u URL, --url URL     URL to fetch cf_clearance cookie from
-  -f FILE, --file FILE  File to write the cf_clearance cookie value to
+  -f FILE, --file FILE  File to write the cf_clearance cookie to
   -t TIMEOUT, --timeout TIMEOUT
-                        Cookie fetch timeout (milliseconds)
+                        Cookie fetch timeout (seconds)
   -p PROXY, --proxy PROXY
-                        Proxy server to use for requests (Authentification not
-                        supported). Example: socks5://172.66.43.144:1080
+                        Proxy server to use for requests (SOCKS5 proxy authentication not supported). Example: socks5://172.66.43.144:1080 or http://username:password@172.66.43.144:1080
 ```
 ```
 Example: python main.py -u https://nowsecure.nl -f cookies.txt -v
--- main.py
+++ main.py
@@ -1,15 +1,85 @@
 import argparse
-import asyncio
+import re
 import sys
 
-import requests
-from playwright.async_api import async_playwright
+import httpx
+from playwright._impl._api_types import TimeoutError
+from playwright.sync_api import sync_playwright
+
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
+)
 
 
-async def main():
-    # Edit the user agent to match the user agent being used within the requests that use the clearance cookie
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+def detect_challenge(html: str) -> bool:
+    challenge_html = (
+        "<title>Please Wait... | Cloudflare</title>",
+        "<title>Just a moment...</title>",
+        "Checking your browser before accessing",
+    )
 
+    return any(x in html for x in challenge_html)
+
+
+def parse_proxy(args: argparse.Namespace) -> dict:
+    if "@" in args.proxy:
+        protocol = args.proxy.split("://")[0]
+        server = f'{protocol}://{args.proxy.split("@")[1]}'
+
+        username = args.proxy.split("@")[0].split("://")[1].split(":")[0]
+        password = args.proxy.split("@")[0].split("://")[1].split(":")[1]
+
+        proxy = {
+            "server": server,
+            "username": username,
+            "password": password,
+        }
+    else:
+        proxy = {"server": args.proxy}
+
+    return proxy
+
+
+def browser(args: argparse.Namespace) -> dict:
+    with sync_playwright() as p:
+        if args.verbose:
+            print("[+] Launching headless browser...")
+
+        if args.proxy:
+            browser = p.firefox.launch(headless=True, proxy=parse_proxy(args))
+        else:
+            browser = p.firefox.launch(headless=True)
+
+        ms_timeout = args.timeout * 1000
+
+        try:
+            context = browser.new_context(user_agent=USER_AGENT)
+            context.set_default_timeout(ms_timeout)
+            page = context.new_page()
+            page.goto(args.url)
+        except Exception as e:
+            if args.verbose:
+                sys.exit("[!] {}".format(str(e).split("\n")[0]))
+
+        verify_button_text = "Verify\s(I|you)\s(am|are)\s(not\sa\sbot|(a\s)?human)"
+        verify_button = page.locator(f"text=/{verify_button_text}/")
+
+        try:
+            while detect_challenge(page.content()):
+                page.wait_for_load_state("networkidle")
+
+                with page.expect_navigation():
+                    if re.search(verify_button_text, page.content()):
+                        verify_button.click()
+                    else:
+                        page.reload()
+        except TimeoutError:
+            pass
+
+        return page.context.cookies()
+
+
+def main() -> None:
     parser = argparse.ArgumentParser(
         description="Fetches cf_clearance cookies from websites issuing cloudflare challenges to users"
     )
@@ -26,118 +96,92 @@ async def main():
     parser.add_argument(
         "-f",
         "--file",
-        help="File to write the cf_clearance cookie value to",
+        help="File to write the cf_clearance cookie to",
         type=str,
         default=None,
     )
     parser.add_argument(
         "-t",
         "--timeout",
-        help="Cookie fetch timeout (milliseconds)",
+        help="Cookie fetch timeout (seconds)",
         type=int,
-        default=5000,
+        default=5,
     )
     parser.add_argument(
         "-p",
         "--proxy",
-        help="Proxy server to use for requests (Authentification not supported). Example: socks5://172.66.43.144:1080",
+        help="Proxy server to use for requests (SOCKS5 proxy authentication not supported). Example: socks5://172.66.43.144:1080 or http://username:password@172.66.43.144:1080",
         type=str,
         default=None,
     )
     args = parser.parse_args()
 
-    if args.url is None:
+    if not args.url:
         sys.exit(parser.print_help())
 
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate",
+        "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-us",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": user_agent,
+        "User-Agent": USER_AGENT,
     }
 
     try:
-        if args.proxy is not None:
-            proxy = {"http": args.proxy, "https": args.proxy}
-            init_request = requests.get(args.url, headers=headers, proxies=proxy)
+        if args.proxy:
+            with httpx.Client(
+                http2=True,
+                follow_redirects=True,
+                timeout=args.timeout,
+                proxies=args.proxy,
+            ) as client:
+                init_request = client.get(args.url, headers=headers)
         else:
-            init_request = requests.get(args.url, headers=headers)
+            with httpx.Client(
+                http2=True, follow_redirects=True, timeout=args.timeout
+            ) as client:
+                init_request = client.get(args.url, headers=headers)
     except Exception as e:
-        sys.exit(f"[!] {e}" if args.verbose is True else None)
+        sys.exit(f"[!] {e}" if args.verbose else None)
 
-    cf_challenge_html = [
-        "<title>Please Wait... | Cloudflare</title>",
-        "<title>Attention Required! | Cloudflare</title>",
-        "Checking your browser before accessing",
-    ]
+    if "<title>Attention Required! | Cloudflare</title>" in init_request.text:
+        sys.exit("[!] Cloudflare returned a CAPTCHA page" if args.verbose else None)
 
-    if any(x in init_request.text for x in cf_challenge_html):
-        if args.verbose is True:
+    if detect_challenge(init_request.text):
+        if args.verbose:
             print(
                 "[+] Cloudflare challenge detected. Attempting to fetch cf_clearance cookie..."
             )
     else:
         sys.exit(
             "[!] Cloudflare challenge not detected. Exiting..."
-            if args.verbose is True
+            if args.verbose
             else None
         )
 
-    async with async_playwright() as p:
-        if args.verbose is True:
-            print("[+] Launching headless browser...")
+    cookies = browser(args)
+    cookie_value = "".join(
+        cookie["value"] for cookie in cookies if cookie["name"] == "cf_clearance"
+    )
 
-        if args.proxy is not None:
-            browser = await p.webkit.launch(headless=True, proxy={"server": args.proxy})
-        else:
-            browser = await p.webkit.launch(headless=True)
-
-        try:
-            context = await browser.new_context(user_agent=user_agent)
-            page = await context.new_page()
-            await page.goto(args.url, wait_until="networkidle")
-            await page.wait_for_timeout(args.timeout)
-        except Exception as e:
-            if args.verbose is True:
-                print("[!] {}".format(str(e).split("\n")[0]))
-                print("[+] Closing headless browser...")
-            await browser.close()
-            sys.exit()
-
-        cookies = await page.context.cookies()
-        cookie_value = "".join(
-            cookie["value"] for cookie in cookies if cookie["name"] == "cf_clearance"
+    if not cookie_value:
+        sys.exit(
+            "[!] Failed to retrieve cf_clearance cookie." if args.verbose else None
         )
 
-        if len(cookie_value) != 0:
-            if args.verbose is True:
-                print(f"[+] Cookie: cf_clearance=" + cookie_value)
-            elif args.verbose is False:
-                print(cookie_value)
+    if args.verbose:
+        print(f"[+] Cookie: cf_clearance={cookie_value}")
+    elif not args.verbose:
+        print(f"cf_clearance={cookie_value}")
 
-            if args.file is not None:
-                try:
-                    with open(args.file, "a") as file:
-                        if args.verbose is True:
-                            print(
-                                f"[+] Writing cf_clearance cookie value to {args.file}..."
-                            )
-                        file.write(cookie_value + "\n")
-                except Exception as e:
-                    print(f"[!] {e}")
-        else:
-            if args.verbose is True:
-                print("[!] Failed to retrieve cf_clearance cookie.")
+    if args.file:
+        if args.verbose:
+            print(f"[+] Writing cf_clearance cookie value to {args.file}...")
 
-        if args.verbose is True:
-            print("[+] Closing headless browser...")
-        await browser.close()
+        with open(args.file, "a") as file:
+            file.write(f"cf_clearance={cookie_value}\n")
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        sys.exit()
+    main()
