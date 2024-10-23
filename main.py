8cf5c6a7626872e78f756f8117cecd7a79b50a34
Xewdy444 <xewdy@xewdy.systems>
2024-10-23 18:54:07 -0500

    Updated default user agent

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ def main() -> None:
     parser.add_argument(
         "-ua",
         "--user-agent",
-        default="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
+        default="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
         help="The user agent to use for the browser requests",
         type=str,
     )
