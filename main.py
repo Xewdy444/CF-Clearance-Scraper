0c26882cc5f67358b35778fc69818983e52f21c5
Nhyalgotphores <47475665+Nhyalgotphores@users.noreply.github.com>
2025-11-24 16:19:32 +0100

    Update main.py
    I don't have idea how it wants me to format it

--- main.py
+++ main.py
@@ -197,8 +197,10 @@ class CloudflareSolver:
 
         while (
             self.extract_clearance_cookie(self.cookies) is None
-            and (self.detect_challenge() is not None
-                or (datetime.now() - start_timestamp).seconds < self._timeout)
+            and (
+                self.detect_challenge() is not None
+                or (datetime.now() - start_timestamp).seconds < self._timeout
+                )
             and (datetime.now() - start_timestamp).seconds < self._timeout
         ):
             if challenge_spinner.is_visible():
